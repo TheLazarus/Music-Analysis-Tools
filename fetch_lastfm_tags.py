13af2eb39dfc0f27f40a37b1e9a0ab5052e6b846
lazzarus <ss22072000@gmail.com>
2021-11-16 19:00:23 +0530

    Fixed some bugs

--- fetch_lastfm_tags.py
+++ fetch_lastfm_tags.py
@@ -3,10 +3,10 @@ import openpyxl
 import os
 import os.path
 
-API_KEY = ""  
-API_SECRET = ""
-USERNAME = ""
-PASSWORD_HASH = pylast.md5("")
+API_KEY = "dcc5bf7ecee79d43278e2b73781a9c26"  
+API_SECRET = "601291923fd46dda602de733f497cc17"
+USERNAME = "laazarus"
+PASSWORD_HASH = pylast.md5("6w5T-FgJH8fB.fx")
 
 def setupLast(API_KEY, API_SECRET, username, password_hash):
     network = pylast.LastFMNetwork(
@@ -27,38 +27,45 @@ def findGenreByTrackOrArtist(type, artist, track):
     runningWeight = 0
     if(type == 'TR'):
         topItems = track.get_top_tags(limit=None)
-        for topItem in topItems:
-            allGenreList.append((topItem.item.get_name(), topItem.weight))
-            # print(topItem.item.get_name(), topItem.weight)
-        for i in range(1,len(allGenreList)):
-            totalWeight += int(allGenreList[i][1])
-        for i in range(1, len(allGenreList)):
-            if(runningWeight/totalWeight <= 0.95):
-                runningWeight += int(allGenreList[i][1])
-                genreList.append(allGenreList[i][0])
-            else:
-                break
-        return genreList[1]
+        if(len(topItems) == 0):
+            raise ValueError("No info found for this track")
+        else:
+            for topItem in topItems:
+                allGenreList.append((topItem.item.get_name(), topItem.weight))
+                print(topItem.item.get_name(), topItem.weight)
+            for i in range(1,len(allGenreList)):
+                totalWeight += int(allGenreList[i][1])
+            for i in range(1, len(allGenreList)):
+                if(runningWeight/totalWeight <= 0.90):
+                    runningWeight += int(allGenreList[i][1])
+                    genreList.append(allGenreList[i][0])
+                else:
+                    break
+            return genreList
     elif(type == 'AR'):
         topArtistItems = artist.get_top_tags(limit=None)
-        for topItem in topArtistItems:
-                allGenreList.append((topItem.item.get_name(), topItem.weight))
-                # print(topItem.item.get_name(), topItem.weight)
-        for i in range(1,len(allGenreList)):
-            totalWeight += int(allGenreList[i][1])
-        for i in range(1, len(allGenreList)):
-            if(runningWeight/totalWeight <= 0.95):
-                runningWeight += int(allGenreList[i][1])
-                genreList.append(allGenreList[i][0])
-            else:
-                break
-        return genreList[1]
+        if(len(topArtistItems) == 0):
+            raise ValueError("No info found for this artist")
+        else:
+            for topItem in topArtistItems:
+                    allGenreList.append((topItem.item.get_name(), topItem.weight))
+                    print(topItem.item.get_name(), topItem.weight)
+            for i in range(1,len(allGenreList)):
+                totalWeight += int(allGenreList[i][1])
+            for i in range(1, len(allGenreList)):
+                if(runningWeight/totalWeight <= 0.90):
+                    runningWeight += int(allGenreList[i][1])
+                    genreList.append(allGenreList[i][0])
+                else:
+                    break
+            return genreList
 
 def findGenre(PATH):
     STARTING_ROW = 3
     ARTIST_COLUMN = 3
-    LASTFM_GENRE_COLUMN = 5
-    LASTFM_INFO_COLUMN = 6
+    TOTAL_ROWS_COLUMN = 5
+    LASTFM_GENRE_COLUMN = 6
+    LASTFM_INFO_COLUMN = 7
     TRACK_COLUMN = 2
     for root, dirs, files in os.walk(PATH):
         for File in files:
@@ -72,18 +79,17 @@ def findGenre(PATH):
                 totalQueriedByTrack = 0
                 totalQueriedByArtist = 0
                 lastfm_info_cell = sheet.cell(row=STARTING_ROW, column=LASTFM_INFO_COLUMN)
-                for row in range(STARTING_ROW, sheet.max_row+1):
-                    
-                    lastfm_genre_cell = sheet.cell(row=row, column=LASTFM_GENRE_COLUMN)
-                    
+                totalRows = int(sheet.cell(row=STARTING_ROW, column=TOTAL_ROWS_COLUMN).value)+1
+                for row in range(STARTING_ROW, totalRows):
                     try:
+                        lastfm_genre_cell = sheet.cell(row=row, column=LASTFM_GENRE_COLUMN)
                         artist = sheet.cell(row=row, column=ARTIST_COLUMN).value
                         track = sheet.cell(row=row, column=TRACK_COLUMN).value
                         print(artist, track)
                         lastfm_artist = network.get_artist(artist)
                         lastfm_track = network.get_track(title=track, artist=artist)
                         lastfm_genreByTrack = findGenreByTrackOrArtist('TR', lastfm_artist, lastfm_track)
-                        lastfm_genre_cell.value = f'TR {lastfm_genreByTrack}'
+                        lastfm_genre_cell.value = f'{lastfm_genreByTrack}'
                         wb.save(dataFilePath)
                         totalQueried += 1
                         totalQueriedByTrack += 1
@@ -91,7 +97,7 @@ def findGenre(PATH):
                     except:
                         try:
                             lastfm_genreByArtist = findGenreByTrackOrArtist('AR', lastfm_artist, lastfm_track)
-                            lastfm_genre_cell.value = f'AL {lastfm_genreByArtist}'
+                            lastfm_genre_cell.value = f'{lastfm_genreByArtist}'
                             wb.save(dataFilePath)
                             totalQueried += 1
                             totalQueriedByArtist += 1
@@ -106,7 +112,6 @@ def findGenre(PATH):
                 lastfm_info_cell.value = f'Total Song Genres Found - {totalQueried} / {sheet.max_row - STARTING_ROW + 1}, Found By Track - {totalQueriedByTrack}, Found By Artist - {totalQueriedByArtist}'
                 wb.save(dataFilePath)
             except:
-                lastfm_genre_cell = 'N/A File Error'
                 wb.save(dataFilePath)
                 print("Error Opening File")
-findGenre("")
+findGenre("../fingerprinter/GTZAN-Last.fm Genres/")
